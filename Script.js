3447e1f8e676ac10f214dd61e1843671533f4f55
Diogo Carvalho <82677312+DioCar84@users.noreply.github.com>
2021-11-03 10:46:58 +0000

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,31 +1,33 @@
 //importing the class so we can create objects
 import MortgageCalculator from "./MortgageCalculator.js";
 
-/* creates principal variable from the user input in "principal" text field */
-var principal = parseInt(document.getElementById("principal").value);
-/* creates interestRate variable from the user input in "interestRate" text field */
-var interestRate = parseInt(document.getElementById("interestRate").value);
-/* creates duration variable from the user input in "duration" text field */
-var duration = parseInt(document.getElementById("duration").value);
-/* creates payments variable for the "payments" text field */
-var payments = document.getElementById("payments");
-/* creates button variable from the "Calculate Monthly Payments" button */
-var button = document.querySelectorAll("input")[3];
+/* allows the DOM content to load without waiting for CSS or other factors to be read */
+document.addEventListener("DOMContentLoaded", function() {
+    /* creates principal variable from the user input in "principal" text field */
+    var principal = parseInt(document.getElementById("principal").value);
+    /* creates interestRate variable from the user input in "interestRate" text field */
+    var interestRate = parseInt(document.getElementById("interestRate").value);
+    /* creates duration variable from the user input in "duration" text field */
+    var duration = parseInt(document.getElementById("duration").value);
+    /* creates payments variable for the "payments" text field */
+    var payments = document.getElementById("payments");
+    /* creates button variable from the "Calculate Monthly Payments" button */
+    var button = document.querySelectorAll("input")[3];
 
 
-/* adds click event listener to the "Calculate Monthly Payments" button to perform mortgage calculation on click */
-button.addEventListener("click", function() {
-    /* refreshes principle variable to make sure it's updated and turns the String value into an integer*/
-    principal = parseInt(document.getElementById("principal").value);
-    /* refreshes interestRate variable to make sure it's updated and turns the String value into a float with 2 decimal places*/
-    interestRate = parseInt(document.getElementById("interestRate").value);
-    /* refreshes duration variable to make sure it's updated and turns the String value into an integer*/
-    duration = parseInt(document.getElementById("duration").value);
-    /* creats a new MortgageCalculator object and stores it in the caclculator variable */
-    var calculator = new MortgageCalculator(principal, duration, interestRate);
-    /* performs the caculateMortgage function and displays it to the user in the "payments" ext field */
-    payments.value = "$" + parseFloat(calculator.calculateMortgage().toFixed(2));
+    /* adds click event listener to the "Calculate Monthly Payments" button to perform mortgage calculation on click */
+    button.addEventListener("click", function() {
+        /* refreshes principle variable to make sure it's updated and turns the String value into an integer*/
+        principal = parseInt(document.getElementById("principal").value);
+        /* refreshes interestRate variable to make sure it's updated and turns the String value into a float with 2 decimal places*/
+        interestRate = parseInt(document.getElementById("interestRate").value);
+        /* refreshes duration variable to make sure it's updated and turns the String value into an integer*/
+        duration = parseInt(document.getElementById("duration").value);
+        /* creats a new MortgageCalculator object and stores it in the caclculator variable */
+        var calculator = new MortgageCalculator(principal, duration, interestRate);
+        /* performs the caculateMortgage function and displays it to the user in the "payments" ext field */
+        payments.value = "$" + parseFloat(calculator.calculateMortgage().toFixed(2));
+    });
+
 });
-
-
 
